49371ce9592299ce33c298baa17f9e80ccc217be
naganoaislab-dev <306069025+naganoaislab-dev@users.noreply.github.com>
2026-07-22 18:16:20 +0900

    Add budget return flow

--- sw.js
+++ sw.js
@@ -4,15 +4,15 @@ const SCOPE_KEY = new URL(self.registration.scope).pathname.replace(/[^a-z0-9]+/
 const IS_LEGACY_VERSIONED_WORKER = new URL(self.location.href).searchParams.has("v");
 const LEGACY_SCOPED_CACHE_PATTERN = new RegExp(`^budget-minus-${SCOPE_KEY}-v\\d+$`);
 const CACHE_PREFIX = `budget-minus-${SCOPE_KEY}--`;
-const CACHE_VERSION = `${CACHE_PREFIX}v49`;
+const CACHE_VERSION = `${CACHE_PREFIX}v50`;
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=49",
-  "./app.js?v=49",
-  "./db.js?v=49",
-  "./manifest.webmanifest?v=49",
-  "./icons/icon.svg?v=49"
+  "./styles.css?v=50",
+  "./app.js?v=50",
+  "./db.js?v=50",
+  "./manifest.webmanifest?v=50",
+  "./icons/icon.svg?v=50"
 ];
 
 function scopedUrl(path) {
